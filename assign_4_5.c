22a95e5a8f74e7e803d9f3266d5c06a7c4e59ca5
sorencoder <69846992+sorencoder@users.noreply.github.com>
2020-12-10 19:03:23 +0530

    Create assign_4_5.c

---
+++ assign_4_5.c
@@ -0,0 +1,25 @@
+//find out square root of a number given by the user without using sqrt function
+#include<stdio.h>
+void main()
+{
+    int number;
+    float temp, sqrt;
+    printf("Enter a  numberto find the square root: ");
+    scanf("%d", &number);
+    // store the half of the given number e.g from 256 => 128
+    sqrt = number / 2;
+    temp = 0;
+    // Iterate until sqrt is different of temp, that is updated on the loop
+    while(sqrt != temp){
+        // initially 0, is updated with the initial value of 128
+        // (on second iteration = 65)
+        // and so on
+        temp = sqrt;
+        // Then, replace values (256 / 128 + 128 ) / 2 = 65
+        // (on second iteration 34.46923076923077)
+        // and so on
+        sqrt = ( number/temp + temp) / 2;
+    }
+
+    printf("The square root of %d is %.2f", number, sqrt);
+}
